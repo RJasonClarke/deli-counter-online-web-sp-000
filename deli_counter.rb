2b7af776f8e0d7c29ac9f21ca56f3033c5ca20d6
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-21 00:18:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,5 @@
-def line(katz_deli)
+def line(array)
   array = []
   count = 0
   puts "The line is currently empty."
-end
-
-def line(other_deli)
-  puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
 end
