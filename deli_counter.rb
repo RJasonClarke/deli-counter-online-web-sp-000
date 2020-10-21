276444949f11367225908aa58bac677ef12b7ecb
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-21 00:03:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,10 @@
 katz_deli = []
 
-def line(people)
-  puts "The line is currently empty."
+def line
+  if katz_deli.length == 0
+  return "The line is currently empty."
+else
+  katz_deli.each do |name|
 end
 
 def take_a_number(katz_deli, puts)
