f08ff23daa4aad2afcb58cd4ca5dd39f3ec0bf76
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-21 00:22:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 def line(array)
   if array.length == 0
   puts "The line is currently empty."
-else
+  else
   message = "The line is currently:"
 
   array.each_with_index do |value, index|
     message += " #{index.to_i+1}. #{value}"
-end
-puts "#{message}"
-end
+  end
+  puts "#{message}"
+  end
 end
